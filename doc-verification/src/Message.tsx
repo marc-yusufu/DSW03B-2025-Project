c3df2c72943fd4ce990b0bc3ddf25b04d6dc94bf
Marc <marcyusufu3@gmail.com>
2025-05-15 08:20:42 +0200

    Start of front-end

---
+++ doc-verification/src/Message.tsx
@@ -0,0 +1,16 @@
+
+
+function Message(){
+    
+    const name = "";
+    if(name){
+        return(
+            <h1>Hello {name}</h1>
+        )
+    }
+    return (
+        <h1>Hello world</h1>
+    );
+}
+
+export default Message;
